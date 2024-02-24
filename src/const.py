e09325817db40a7e972207fb3abda37585efc88f
host <nikproplay07@mail.ru>
2024-02-24 16:52:46 +0800

    da

--- src/const.py
+++ src/const.py
@@ -2,7 +2,7 @@ from pygame.colordict import THECOLORS as COLORS
 
 W_HEIGHT, W_WIDTH = 1000, 1200
 FPS = 60
-N, M = 40, 40
+N, M = 60, 60
 C_HEIGHT, C_WIDTH = W_HEIGHT / N, W_WIDTH / M
 
 MURDER_C, ALIVE_C = COLORS["black"], COLORS['white']
